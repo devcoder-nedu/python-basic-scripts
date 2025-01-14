05a45961a78a360c274e505b59814c80f5cc5617
Chinedu Mgbemena <mgbemena.nedu@gmail.com>
2025-01-14 13:41:10 +0100

    added the rock paper scissors script (1st script)

---
+++ rockpaperscissors.py
@@ -0,0 +1,34 @@
+import random
+
+print('ROCK, PAPER, SCISSORS')
+wins = 0
+losses = 0
+ties = 0
+
+pattern_to_win = {'p':'r', 'r':'s', 'scissors':'p'}
+
+
+
+while True:
+    player_choice = input('Enter your move: (r)ock, (p)aper, (s)cissors or (q)uit: ')
+    if player_choice == 'q':
+        break
+    elif player_choice not in ['r', 'p', 's']:
+        print('Please enter a valid move.')
+        continue
+    else:
+        computer_choice = random.choice(['r', 'p', 's'])
+        print('Computer chose:', computer_choice)
+        if player_choice == computer_choice:
+            print('It is a tie!')
+            ties += 1
+        elif pattern_to_win[player_choice] == computer_choice:
+            print('You win!')
+            wins += 1
+        else:  
+            # computer wins
+            print('You lose!')
+            losses += 1
+        print(f'Wins: {wins}, Losses: {losses}, Ties: {ties}')
+        continue
+print('Goodbye!')
